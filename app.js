9be3b47c8bfd5990380b4a8e3709c7b5984eb12a
zarzeroGuimar <arielguimar@gmail.com>
2026-05-06 09:29:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,65 @@
+const UART_SERVICE_UUID =
+  '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
 
+const UART_TX_CHARACTERISTIC_UUID =
+  '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+
+let txCharacteristic = null;
+
+document
+  .getElementById('connectBtn')
+  .addEventListener('click', connectMicrobit);
+
+async function connectMicrobit() {
+
+  try {
+
+    const device =
+      await navigator.bluetooth.requestDevice({
+        filters: [
+          { namePrefix: 'BBC micro:bit' }
+        ],
+        optionalServices: [UART_SERVICE_UUID]
+      });
+
+    const server = await device.gatt.connect();
+
+    const service =
+      await server.getPrimaryService(
+        UART_SERVICE_UUID
+      );
+
+    txCharacteristic =
+      await service.getCharacteristic(
+        UART_TX_CHARACTERISTIC_UUID
+      );
+
+    document.getElementById('status')
+      .innerText = '✅ Conectado';
+
+  } catch (error) {
+
+    console.error(error);
+
+    document.getElementById('status')
+      .innerText = '❌ Error de conexión';
+  }
+}
+
+async function sendCommand(command) {
+
+  if (!txCharacteristic) {
+
+    alert('Primero conecta la micro:bit');
+    return;
+  }
+
+  const encoder = new TextEncoder();
+
+  const data =
+    encoder.encode(command + '\n');
+
+  await txCharacteristic.writeValue(data);
+
+  console.log('Enviado:', command);
+}
