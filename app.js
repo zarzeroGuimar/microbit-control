9c4c4d58fbdfbf83fee0774d5d3e96c38819cdb9
zarzeroGuimar <arielguimar@gmail.com>
2026-05-06 16:33:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,69 +1,88 @@
 const UART_SERVICE_UUID =
-  '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
+'6e400001-b5a3-f393-e0a9-e50e24dcca9e';
 
-const UART_TX_CHARACTERISTIC_UUID =
-  '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+const UART_RX_CHARACTERISTIC_UUID =
+'6e400002-b5a3-f393-e0a9-e50e24dcca9e';
 
-let txCharacteristic = null;
+let rxCharacteristic = null;
 
 document
-  .getElementById('connectBtn')
-  .addEventListener('click', connectMicrobit);
+.getElementById('connectBtn')
+.addEventListener('click', connectMicrobit);
 
 async function connectMicrobit() {
 
   try {
 
     const device =
-      await navigator.bluetooth.requestDevice({
-        filters: [
-          { namePrefix: 'BBC micro:bit' }
-        ],
-        optionalServices: [UART_SERVICE_UUID]
-      });
+    await navigator.bluetooth.requestDevice({
 
-    const server = await device.gatt.connect();
+      filters: [{
+        services: [UART_SERVICE_UUID]
+      }]
+
+    });
+
+    console.log("Dispositivo encontrado");
+
+    const server =
+    await device.gatt.connect();
+
+    console.log("GATT conectado");
 
     const service =
-      await server.getPrimaryService(
-        UART_SERVICE_UUID
-      );
+    await server.getPrimaryService(
+      UART_SERVICE_UUID
+    );
 
-    txCharacteristic =
-      await service.getCharacteristic(
-        UART_TX_CHARACTERISTIC_UUID
-      );
+    console.log("Servicio UART OK");
+
+    rxCharacteristic =
+    await service.getCharacteristic(
+      UART_RX_CHARACTERISTIC_UUID
+    );
+
+    console.log("Característica RX OK");
 
     document.getElementById('status')
-      .innerText = '✅ Conectado';
+    .innerHTML = '✅ Conectado';
 
-  } catch (error) {
+  }
+
+  catch(error) {
 
     console.error(error);
 
     document.getElementById('status')
-      .innerText = '❌ Error de conexión';
+    .innerHTML =
+    '❌ Error conexión';
+
   }
+
 }
 
-async function sendCommand(command){
+async function sendCommand(command) {
 
   if(!rxCharacteristic){
 
-    alert('Conecta primero');
+    alert('Primero conecta la micro:bit');
     return;
   }
 
-  try{
+  try {
 
-    const encoder = new TextEncoder();
+    const encoder =
+    new TextEncoder();
 
     const data =
     encoder.encode(command + '\n');
 
     await rxCharacteristic.writeValue(data);
 
-    console.log(command);
+    console.log(
+      'Comando enviado:',
+      command
+    );
 
   }
 
